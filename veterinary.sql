8edf9c3fc7a5e5c82807ef24c56b9b4793215e05
Naja-Ysabel <capilinajaysabel@gmail.com>
2025-12-01 10:37:07 +0800

    feat/list-animal-w-most-appoinment

--- veterinary.sql
+++ veterinary.sql
@@ -164,11 +164,17 @@ FROM
 JOIN
     animals a ON o.ownerid = a.ownerid;
 
-SELECT an.name, COUNT(*) AS total_appointments
+SELECT LOWER(an.name) AS name
 FROM appointments a
 JOIN animals an ON a.animalid = an.animalid
 GROUP BY an.name
-ORDER BY total_appointments DESC
-LIMIT 1;
+HAVING COUNT(*) = (
+    SELECT MAX(app_count)
+    FROM (
+        SELECT COUNT(*) AS app_count
+        FROM appointments
+        GROUP BY animalid
+    ) sub
+);
 
 
